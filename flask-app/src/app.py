49ca8c2dd02696a5ddef5b6e653079317f960bb9
Ignacio Millan <ignacio@awardforce.com>
2021-10-25 15:25:02 +0200

    envvars

--- flask-app/src/app.py
+++ flask-app/src/app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 
-    return f'''Hey, we have Flask in a Docker container!!!! no cache'''
+    # Leer variables de entorno
+    foo = os.getenv('FOO', "DEFAULT")
+
+    return f'''Hey, we have Flask in a Docker container!!!! 
+    <br> envvar: {foo} 
+    '''
 
 app.run(debug=True, host='0.0.0.0')
